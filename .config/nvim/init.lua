9f4f3aeaa1fb5a0428f425f427c8af679ac5aa88
shemishtamesh <shemishtamail@gmail.com>
2023-10-16 20:14:40 +0300

    use logseq

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("general") -- general settings
-require("highlights") -- colourscheme and other highlights
+-- require("commands") -- general commands
 require("keymaps") -- change keymaps
+require("highlights") -- colourscheme and other highlights
 require("packer_bootstrap") -- packer auto-installer
 require("plugins") -- use plugins
 require("vimwiki") -- configure vimwiki
